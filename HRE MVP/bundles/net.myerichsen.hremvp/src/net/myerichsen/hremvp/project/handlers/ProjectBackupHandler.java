718dbd47d371462d5c06a495253f6cb0a723da8a
MichaelErichsen <michael.erichsen@xact.dk>
2019-01-27 13:48:03 +0100

    41: New Project does not display in Project Navigator
    Task-Url: https://github.com/MichaelErichsen/MVP/issues/41

--- HRE MVP/bundles/net.myerichsen.hremvp/src/net/myerichsen/hremvp/project/handlers/ProjectBackupHandler.java
+++ HRE MVP/bundles/net.myerichsen.hremvp/src/net/myerichsen/hremvp/project/handlers/ProjectBackupHandler.java
@@ -2,6 +2,9 @@
 package net.myerichsen.hremvp.project.handlers;
 
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.workbench.IWorkbench;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
 
 /**
  * @author Michael Erichsen, &copy; History Research Environment Ltd., 2019
@@ -13,8 +16,10 @@ public class ProjectBackupHandler {
 	 * 
 	 */
 	@Execute
-	public void execute() {
-
+	public void execute(IWorkbench workbench, Shell shell) {
+		MessageDialog dialog = new MessageDialog(shell, "Backup", null, "Not yet implemented",
+				MessageDialog.INFORMATION, 0, new String[] { "OK" });
+		dialog.open();
 	}
 
 }
--- HRE MVP/bundles/net.myerichsen.hremvp/src/net/myerichsen/hremvp/project/handlers/ProjectRestoreHandler.java
+++ HRE MVP/bundles/net.myerichsen.hremvp/src/net/myerichsen/hremvp/project/handlers/ProjectRestoreHandler.java
@@ -2,11 +2,25 @@
 package net.myerichsen.hremvp.project.handlers;
 
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.workbench.IWorkbench;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
 
+/**
+ * @author  Michael Erichsen, &copy; History Research Environment Ltd., 2019
+ * @version 27. jan. 2019
+ *
+ */
 public class ProjectRestoreHandler {
+	/**
+	 * @param workbench
+	 * @param shell
+	 */
 	@Execute
-	public void execute() {
-		
+	public void execute(IWorkbench workbench, Shell shell) {
+		MessageDialog dialog = new MessageDialog(shell, "Restore", null, "Not yet implemented",
+				MessageDialog.INFORMATION, 0, new String[] { "OK" });
+		dialog.open();
 	}
 		
 }
