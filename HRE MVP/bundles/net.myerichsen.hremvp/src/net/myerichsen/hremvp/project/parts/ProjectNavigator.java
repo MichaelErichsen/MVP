28694b5c7589b1b94cbd2098d98763f6e83ad1ff
MichaelErichsen <michael.erichsen@xact.dk>
2019-02-16 13:22:05 +0100

    65: Move context menus from application model to part source. New Project
    Task-Url: https://github.com/MichaelErichsen/MVP/issues/65

--- HRE MVP/bundles/net.myerichsen.hremvp/src/net/myerichsen/hremvp/project/parts/ProjectNavigator.java
+++ HRE MVP/bundles/net.myerichsen.hremvp/src/net/myerichsen/hremvp/project/parts/ProjectNavigator.java
@@ -1,7 +1,13 @@
 package net.myerichsen.hremvp.project.parts;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Date;
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.annotation.PostConstruct;
@@ -13,7 +19,15 @@ import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.core.services.events.IEventBroker;
 import org.eclipse.e4.ui.di.Focus;
 import org.eclipse.e4.ui.di.UIEventTopic;
+import org.eclipse.e4.ui.model.application.MApplication;
+import org.eclipse.e4.ui.model.application.ui.basic.MBasicFactory;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.e4.ui.model.application.ui.basic.MPartStack;
+import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
 import org.eclipse.e4.ui.services.EMenuService;
+import org.eclipse.e4.ui.workbench.modeling.EModelService;
+import org.eclipse.e4.ui.workbench.modeling.EPartService;
+import org.eclipse.e4.ui.workbench.modeling.EPartService.PartState;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.TableViewer;
@@ -28,12 +42,13 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
-import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.swt.widgets.Text;
 
 import com.opcoach.e4.preferences.ScopedPreferenceStore;
@@ -42,6 +57,10 @@ import net.myerichsen.hremvp.Constants;
 import net.myerichsen.hremvp.HreH2ConnectionPool;
 import net.myerichsen.hremvp.MvpException;
 import net.myerichsen.hremvp.filters.NavigatorFilter;
+import net.myerichsen.hremvp.project.dialogs.ProjectNameSummaryDialog;
+import net.myerichsen.hremvp.project.models.ProjectList;
+import net.myerichsen.hremvp.project.models.ProjectModel;
+import net.myerichsen.hremvp.project.providers.ProjectNewDatabaseProvider;
 import net.myerichsen.hremvp.project.providers.ProjectProvider;
 import net.myerichsen.hremvp.providers.HREColumnLabelProvider;
 
@@ -49,7 +68,7 @@ import net.myerichsen.hremvp.providers.HREColumnLabelProvider;
  * Navigator part to display and maintain all HRE projects
  *
  * @author Michael Erichsen, &copy; History Research Environment Ltd., 2018-2019
- * @version 15. feb. 2019
+ * @version 16. feb. 2019
  *
  */
 public class ProjectNavigator {
@@ -60,6 +79,12 @@ public class ProjectNavigator {
 
 	@Inject
 	private IEventBroker eventBroker;
+	@Inject
+	private EModelService modelService;
+	@Inject
+	private MApplication application;
+	@Inject
+	private EPartService partService;
 
 	private ProjectProvider provider;
 	private TableViewer tableViewer;
@@ -151,7 +176,7 @@ public class ProjectNavigator {
 		mntmNewProject.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				newProject();
+				newProject(parent.getShell());
 			}
 		});
 		mntmNewProject.setText("New project...");
@@ -186,7 +211,6 @@ public class ProjectNavigator {
 			@Override
 			public void keyReleased(KeyEvent e) {
 				navigatorFilter.setSearchText(textNameFilter.getText());
-				LOGGER.fine("Filter string: " + textNameFilter.getText());
 				tableViewer.refresh();
 			}
 		});
@@ -197,7 +221,6 @@ public class ProjectNavigator {
 		tableViewer.setContentProvider(ArrayContentProvider.getInstance());
 		try {
 			tableViewer.setInput(provider.get());
-			LOGGER.fine("Set input");
 		} catch (SQLException | MvpException e) {
 			LOGGER.severe(e.getMessage());
 			e.printStackTrace();
@@ -205,144 +228,118 @@ public class ProjectNavigator {
 	}
 
 	/**
-	 * 
+	 * @param shell
 	 */
-	protected void newProject() {
-		Connection conn = null;
+	protected void newProject(Shell shell) {
+		// Open file dialog
+		final FileDialog dialog = new FileDialog(shell, SWT.SAVE);
+		dialog.setText("Create new HRE Project");
+		dialog.setFilterPath("./");
+		final String[] extensions = { "*.h2.db", "*.mv.db", "*.*" };
+		dialog.setFilterExtensions(extensions);
+		dialog.open();
 
-		// Disconnect from any currently connected database
+		final String shortName = dialog.getFileName();
+		final String[] parts = shortName.split("\\.");
+		final String path = dialog.getFilterPath();
+		final String dbName = parts[0];
+
 		try {
+			// Create the new database
+			LOGGER.fine("New database name: " + path + "\\" + dbName);
+
+			store.setValue("DBPATH", path);
+			store.setValue("DBNAME", dbName);
+
+			ProjectNewDatabaseProvider pndprovider = new ProjectNewDatabaseProvider();
+
+			pndprovider.provide(dbName);
+
+			// Disconnect from any currently connected database
+			Connection conn = null;
+
 			conn = HreH2ConnectionPool.getConnection();
 
 			if (conn != null) {
 				conn.createStatement().execute("SHUTDOWN");
 				conn.close();
-				conn = null;
 				try {
 					HreH2ConnectionPool.dispose();
 				} catch (final Exception e) {
-					LOGGER.info("Already disposed");
+					LOGGER.fine("No connection pool to dispose");
 				}
 			}
+
+			// Connect to the new database
+			conn = HreH2ConnectionPool.getConnection(dbName);
+
+			final String h2Version = store.getString("H2VERSION");
+			LOGGER.fine("Retrieved H2 version from preferences: "
+					+ h2Version.substring(0, 3));
+			PreparedStatement ps;
+
+			if (h2Version.substring(0, 3).equals("1.3")) {
+				ps = conn.prepareStatement(
+						"SELECT TABLE_NAME, 0 FROM INFORMATION_SCHEMA.TABLES "
+								+ "WHERE TABLE_TYPE = 'TABLE' ORDER BY TABLE_NAME");
+			} else {
+				ps = conn.prepareStatement(
+						"SELECT TABLE_NAME, ROW_COUNT_ESTIMATE FROM INFORMATION_SCHEMA.TABLES "
+								+ "WHERE TABLE_TYPE = 'TABLE' ORDER BY TABLE_NAME");
+			}
+
+			ps.executeQuery();
+			conn.close();
+
+			// Open a dialog for summary
+			final ProjectNameSummaryDialog pnsDialog = new ProjectNameSummaryDialog(
+					shell);
+			pnsDialog.open();
+
+			// Update the HRE properties
+			final LocalDateTime now = LocalDateTime.now();
+			final ZonedDateTime zdt = now.atZone(ZoneId.systemDefault());
+			final Date timestamp = Date.from(zdt.toInstant());
+			final ProjectModel model = new ProjectModel(
+					pnsDialog.getProjectName(), timestamp,
+					pnsDialog.getProjectSummary(), "LOCAL",
+					path + "\\" + dbName);
+
+			LOGGER.fine("New properties " + pnsDialog.getProjectName() + " "
+					+ timestamp.toString() + " " + pnsDialog.getProjectSummary()
+					+ " LOCAL " + dbName);
+
+			final int index = ProjectList.add(model);
+
+			// Set database name in title bar
+			final MWindow window = (MWindow) modelService
+					.find("net.myerichsen.hremvp.window.main", application);
+			window.setLabel("HRE MVP v0.2 - " + dbName);
+
+			// Open H2 Database Navigator
+			final MPart h2dnPart = MBasicFactory.INSTANCE.createPart();
+			h2dnPart.setLabel("Database Tables");
+			h2dnPart.setContainerData("650");
+			h2dnPart.setCloseable(true);
+			h2dnPart.setVisible(true);
+			h2dnPart.setContributionURI(
+					"bundleclass://net.myerichsen.hremvp/net.myerichsen.hremvp.databaseadmin.H2DatabaseNavigator");
+			final List<MPartStack> stacks = modelService
+					.findElements(application, null, MPartStack.class, null);
+			stacks.get(stacks.size() - 2).getChildren().add(h2dnPart);
+			partService.showPart(h2dnPart, PartState.ACTIVATE);
+
+			eventBroker.post(Constants.DATABASE_UPDATE_TOPIC, dbName);
+			eventBroker.post(Constants.PROJECT_LIST_UPDATE_TOPIC, index);
+			eventBroker.post(Constants.PROJECT_PROPERTIES_UPDATE_TOPIC, index);
+			eventBroker.post("MESSAGE",
+					"Project database " + dbName + " has been created");
 		} catch (final Exception e1) {
 			eventBroker.post("MESSAGE", e1.getMessage());
 			LOGGER.severe(e1.getMessage());
 			e1.printStackTrace();
 		}
-
-		// Find selected database
-		final TableItem[] selection = tableViewer.getTable().getSelection();
-
-		int childPid = 0;
-		String primaryName = null;
-		if (selection.length > 0) {
-			final TableItem item = selection[0];
-			childPid = Integer.parseInt(item.getText(0));
-			primaryName = item.getText(1);
-		}
-//		int index = 0;
-//
-//		final List<MPartStack> stacks = modelService.findElements(application,
-//				null, MPartStack.class, null);
-//		MPart part = MBasicFactory.INSTANCE.createPart();
-//		boolean found = false;
-//
-//		for (final MPartStack mPartStack : stacks) {
-//			final List<MStackElement> a = mPartStack.getChildren();
-//
-//			for (int i = 0; i < a.size(); i++) {
-//				part = (MPart) a.get(i);
-//				if (part.getContributionURI().equals(contributionURI)) {
-//					final ProjectNavigator pn = (ProjectNavigator) part
-//							.getObject();
-//					index = pn.getTableViewer().getTable().getSelectionIndex();
-//					found = true;
-//					LOGGER.info("Selected index: " + index);
-//					break;
-//				}
-//			}
-//
-//			if (found) {
-//				break;
-//			}
 	}
-
-//	final ProjectModel model = ProjectList.getModel(index);
-//	final String dbName = model.getName();
-//
-//	String path = model.getPath();
-//	File file = new File(path + ".h2.db");if(file.exists())
-//	{
-//		path = file.getParent();
-//	}else
-//	{
-//		file = new File(path + ".mv.db");
-//		if (file.exists()) {
-//			path = file.getParent();
-//		}
-//	}
-//
-//	store.setValue("DBNAME",dbName);store.setValue("DBPATH",path);
-//
-//	try
-//	{
-//		conn = HreH2ConnectionPool.getConnection(dbName);
-//
-//		if (conn != null) {
-//			final String h2Version = store.getString("H2VERSION");
-//			LOGGER.info("Retrieved H2 version from preferences: "
-//					+ h2Version.substring(0, 3));
-//			PreparedStatement ps;
-//
-//			if (h2Version.substring(0, 3).equals("1.3")) {
-//				ps = conn.prepareStatement(
-//						"SELECT TABLE_NAME, 0 FROM INFORMATION_SCHEMA.TABLES "
-//								+ "WHERE TABLE_TYPE = 'TABLE' ORDER BY TABLE_NAME");
-//			} else {
-//				ps = conn.prepareStatement(
-//						"SELECT TABLE_NAME, ROW_COUNT_ESTIMATE FROM INFORMATION_SCHEMA.TABLES "
-//								+ "WHERE TABLE_TYPE = 'TABLE' ORDER BY TABLE_NAME");
-//			}
-//
-//			ps.executeQuery();
-//			conn.close();
-//		}
-//
-//		// Set database name in title bar
-//		final MWindow window = (MWindow) modelService
-//				.find("net.myerichsen.hremvp.window.main", application);
-//		window.setLabel("HRE MVP v0.2 - " + dbName);
-//
-//		// Open H2 Database Navigator
-//		final MPart h2dnPart = MBasicFactory.INSTANCE.createPart();
-//		h2dnPart.setLabel("Database Tables");
-//		h2dnPart.setContainerData("650");
-//		h2dnPart.setCloseable(true);
-//		h2dnPart.setVisible(true);
-//		h2dnPart.setContributionURI(
-//				"bundleclass://net.myerichsen.hremvp/net.myerichsen.hremvp.databaseadmin.H2DatabaseNavigator");
-//		stacks.get(stacks.size() - 2).getChildren().add(h2dnPart);
-//		partService.showPart(h2dnPart, PartState.ACTIVATE);
-//
-//		eventBroker.post(Constants.DATABASE_UPDATE_TOPIC, dbName);
-//
-//		if (index > 0) {
-//			eventBroker.post(Constants.PROJECT_LIST_UPDATE_TOPIC, index + 1);
-//			eventBroker.post(Constants.PROJECT_PROPERTIES_UPDATE_TOPIC,
-//					index + 1);
-//		}
-//
-//		eventBroker.post("MESSAGE",
-//				"Project database " + dbName + " has been opened");
-//	}catch(
-//	final Exception e1)
-//	{
-//		eventBroker.post("MESSAGE", e1.getMessage());
-//		LOGGER.severe(e1.getMessage());
-//		e1.printStackTrace();
-//	}
-//
-//	}
 
 	/**
 	 *
