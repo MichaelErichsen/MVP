635e436fe0f0be35a2e33dd28c5e0e4f0f1e0f8b
MichaelErichsen <michael.erichsen@xact.dk>
2019-04-08 11:15:42 +0200

    109: Debug person view
    Task-Url: https://github.com/MichaelErichsen/MVP/issues/109

--- HRE MVP/bundles/net.myerichsen.hremvp/src/net/myerichsen/hremvp/person/parts/PersonNamePartNavigator.java
+++ HRE MVP/bundles/net.myerichsen.hremvp/src/net/myerichsen/hremvp/person/parts/PersonNamePartNavigator.java
@@ -4,19 +4,15 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 import javax.inject.Inject;
 
-import org.eclipse.core.commands.ParameterizedCommand;
-import org.eclipse.e4.core.commands.ECommandService;
-import org.eclipse.e4.core.commands.EHandlerService;
+import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.core.di.annotations.Optional;
-import org.eclipse.e4.core.services.events.IEventBroker;
-import org.eclipse.e4.ui.di.Focus;
 import org.eclipse.e4.ui.di.UIEventTopic;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.jface.window.Window;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
@@ -30,54 +26,99 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
-import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.swt.widgets.Text;
 
 import net.myerichsen.hremvp.Constants;
+import net.myerichsen.hremvp.HreTypeLabelEditingSupport;
+import net.myerichsen.hremvp.dialogs.DateDialog;
+import net.myerichsen.hremvp.dialogs.DateNavigatorDialog;
 import net.myerichsen.hremvp.person.providers.PersonNamePartProvider;
 import net.myerichsen.hremvp.person.providers.PersonNameProvider;
+import net.myerichsen.hremvp.providers.HDateProvider;
 import net.myerichsen.hremvp.providers.HREColumnLabelProvider;
 
 /**
  * Display all data about a name
  *
  * @author Michael Erichsen, &copy; History Research Environment Ltd., 2018-2019
- * @version 26. mar. 2019
+ * @version 8. apr. 2019
  */
-@SuppressWarnings("restriction")
 public class PersonNamePartNavigator {
 	private static final Logger LOGGER = Logger
 			.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
-	@Inject
-	private IEventBroker eventBroker;
-	@Inject
-	private ECommandService commandService;
-	@Inject
-	private EHandlerService handlerService;
+	private Text textFromDate;
+	private Text textToDate;
 
-	private Text textId;
-	private Text textPersonPid;
-	private Text textNameStylePid;
-	private Text textNameStyleLabel;
-	private Text textDateFrom;
-	private Text textDateTo;
 	private Button btnPrimaryName;
-
-	private final PersonNamePartProvider provider;
 	private TableViewer tableViewer;
 
 	private int personNamePid = 0;
+	private int fromDatePid;
+	private int toDatePid;
+
+	private PersonNamePartProvider provider;
+//	private List<List<String>> stringList;
 
 	/**
 	 * Constructor
 	 *
-	 * @throws Exception An exception that provides information on a database
-	 *                   access error or other errors
-	 *
 	 */
 	public PersonNamePartNavigator() {
 		provider = new PersonNamePartProvider();
+	}
+
+	/**
+	 * @param context
+	 *
+	 */
+	private void browseFromDates(IEclipseContext context) {
+		final DateNavigatorDialog dialog = new DateNavigatorDialog(
+				textFromDate.getShell(), context);
+		if (dialog.open() == Window.OK) {
+			try {
+				fromDatePid = dialog.getHdatePid();
+				final HDateProvider hdp = new HDateProvider();
+				hdp.get(fromDatePid);
+				textFromDate.setText(hdp.getDate().toString());
+			} catch (final Exception e) {
+				LOGGER.log(Level.SEVERE, e.toString(), e);
+			}
+		}
+	}
+
+	/**
+	 *
+	 */
+	private void browseToDates(IEclipseContext context) {
+		final DateNavigatorDialog dialog = new DateNavigatorDialog(
+				textToDate.getShell(), context);
+		if (dialog.open() == Window.OK) {
+			try {
+				toDatePid = dialog.getHdatePid();
+				final HDateProvider hdp = new HDateProvider();
+				hdp.get(toDatePid);
+				textToDate.setText(hdp.getDate().toString());
+			} catch (final Exception e) {
+				LOGGER.log(Level.SEVERE, e.toString(), e);
+			}
+		}
+	}
+
+	/**
+	 *
+	 */
+	private void clearFromDate() {
+		textFromDate.setText("");
+		fromDatePid = 0;
+	}
+
+	/**
+	 *
+	 */
+	private void clearToDate() {
+		textToDate.setText("");
+		toDatePid = 0;
 	}
 
 	/**
@@ -86,71 +127,115 @@ public class PersonNamePartNavigator {
 	 * @param parent The parent composite
 	 */
 	@PostConstruct
-	public void createControls(Composite parent) {
-		parent.setLayout(new GridLayout(3, false));
+	public void createControls(Composite parent, IEclipseContext context) {
+		parent.setLayout(new GridLayout(2, false));
 
-		final Label lblId = new Label(parent, SWT.NONE);
-		lblId.setText("ID");
+		final Label lblPersonNameStyle = new Label(parent, SWT.NONE);
+		lblPersonNameStyle.setText("Person Name Style");
 
-		textId = new Text(parent, SWT.BORDER);
-		textId.setLayoutData(
-				new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-		new Label(parent, SWT.NONE);
-
-		final Label lblPersonId = new Label(parent, SWT.NONE);
-		lblPersonId.setText("Person ID");
-
-		textPersonPid = new Text(parent, SWT.BORDER);
-		textPersonPid.setLayoutData(
-				new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-		new Label(parent, SWT.NONE);
-
-		final Label lblNameStyle = new Label(parent, SWT.NONE);
-		lblNameStyle.setText("Name Style");
-
-		textNameStylePid = new Text(parent, SWT.BORDER);
-		textNameStylePid.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseDoubleClick(MouseEvent e) {
-				openNameStyleView();
-			}
-		});
-		textNameStylePid.setLayoutData(
+		Text textPersonNameStyle = new Text(parent, SWT.BORDER);
+		textPersonNameStyle.setEditable(false);
+		textPersonNameStyle.setLayoutData(
 				new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 
-		textNameStyleLabel = new Text(parent, SWT.BORDER);
-		textNameStyleLabel.setEditable(false);
-		textNameStyleLabel.setLayoutData(
-				new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		final Composite compositeFrom = new Composite(parent, SWT.BORDER);
+		compositeFrom.setLayout(new GridLayout(2, false));
+		compositeFrom.setLayoutData(
+				new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
 
-		final Label lblFromDate = new Label(parent, SWT.NONE);
+		final Label lblFromDate = new Label(compositeFrom, SWT.NONE);
 		lblFromDate.setText("From Date");
 
-		textDateFrom = new Text(parent, SWT.BORDER);
-		new Label(parent, SWT.NONE);
+		textFromDate = new Text(compositeFrom, SWT.BORDER);
+		textFromDate.setLayoutData(
+				new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		textFromDate.setEditable(false);
 
-		final Label lblToDate = new Label(parent, SWT.NONE);
+		final Composite compositeButtonsFrom = new Composite(compositeFrom,
+				SWT.NONE);
+		compositeButtonsFrom.setLayoutData(
+				new GridData(SWT.LEFT, SWT.CENTER, false, false, 2, 1));
+		compositeButtonsFrom.setLayout(new RowLayout(SWT.HORIZONTAL));
+
+		final Button btnNewFrom = new Button(compositeButtonsFrom, SWT.NONE);
+		btnNewFrom.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				getNewFromDate(context);
+			}
+		});
+		btnNewFrom.setText("New");
+
+		final Button btnBrowseFrom = new Button(compositeButtonsFrom, SWT.NONE);
+		btnBrowseFrom.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				browseFromDates(context);
+			}
+		});
+		btnBrowseFrom.setText("Browse");
+
+		final Button btnClearFrom = new Button(compositeButtonsFrom, SWT.NONE);
+		btnClearFrom.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				clearFromDate();
+			}
+		});
+		btnClearFrom.setText("Clear");
+
+		final Composite compositeTo = new Composite(parent, SWT.BORDER);
+		compositeTo.setLayoutData(
+				new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
+		compositeTo.setLayout(new GridLayout(2, false));
+
+		final Label lblToDate = new Label(compositeTo, SWT.NONE);
 		lblToDate.setText("To Date");
 
-		textDateTo = new Text(parent, SWT.BORDER);
-		new Label(parent, SWT.NONE);
-		new Label(parent, SWT.NONE);
+		textToDate = new Text(compositeTo, SWT.BORDER);
+		textToDate.setLayoutData(
+				new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		textToDate.setEditable(false);
+
+		final Composite compositeButtonsTo = new Composite(compositeTo,
+				SWT.NONE);
+		compositeButtonsTo.setLayoutData(
+				new GridData(SWT.LEFT, SWT.CENTER, false, false, 2, 1));
+		compositeButtonsTo.setLayout(new RowLayout(SWT.HORIZONTAL));
+
+		final Button btnNewTo = new Button(compositeButtonsTo, SWT.NONE);
+		btnNewTo.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				getNewToDate(context);
+			}
+		});
+		btnNewTo.setText("New");
+
+		final Button btnBrowseTo = new Button(compositeButtonsTo, SWT.NONE);
+		btnBrowseTo.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				browseToDates(context);
+			}
+		});
+		btnBrowseTo.setText("Browse");
+
+		final Button btnClearTo = new Button(compositeButtonsTo, SWT.NONE);
+		btnClearTo.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				clearToDate();
+			}
+		});
+		btnClearTo.setText("Clear");
 
 		btnPrimaryName = new Button(parent, SWT.CHECK);
 		btnPrimaryName.setText("Primary Name");
 		new Label(parent, SWT.NONE);
 
-		final Label lblPersonNameParts = new Label(parent, SWT.NONE);
-		lblPersonNameParts.setText("Name Parts\r\nDblclk to open");
-
 		tableViewer = new TableViewer(parent, SWT.BORDER | SWT.FULL_SELECTION);
 		final Table tablePersonNameParts = tableViewer.getTable();
-		tablePersonNameParts.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseDoubleClick(MouseEvent e) {
-				openNamePartView();
-			}
-		});
 		tablePersonNameParts.setLinesVisible(true);
 		tablePersonNameParts.setHeaderVisible(true);
 		tablePersonNameParts.setLayoutData(
@@ -184,21 +269,25 @@ public class PersonNamePartNavigator {
 				.getColumn();
 		tblclmnValueFromPart.setWidth(100);
 		tblclmnValueFromPart.setText("Value from Part");
+		tableViewerColumnPart.setEditingSupport(
+				new HreTypeLabelEditingSupport(tableViewer, 3));
 		tableViewerColumnPart.setLabelProvider(new HREColumnLabelProvider(3));
+
+		HREColumnLabelProvider.addEditingSupport(tableViewer);
 
 		tableViewer.setContentProvider(ArrayContentProvider.getInstance());
 		try {
 			tableViewer.setInput(provider.getStringList(personNamePid));
-		} catch (final Exception e1) {
-			LOGGER.log(Level.SEVERE, e1.toString(), e1);
+		} catch (final Exception e2) {
+			LOGGER.log(Level.SEVERE, e2.toString(), e2);
 		}
 
-		final Composite composite = new Composite(parent, SWT.NONE);
-		composite.setLayoutData(
-				new GridData(SWT.RIGHT, SWT.CENTER, false, false, 3, 1));
-		composite.setLayout(new RowLayout(SWT.HORIZONTAL));
+		final Composite compositeButtons = new Composite(parent, SWT.NONE);
+		compositeButtons.setLayoutData(
+				new GridData(SWT.RIGHT, SWT.CENTER, false, false, 2, 1));
 
-		final Button buttonUpdate = new Button(composite, SWT.NONE);
+		final Button buttonUpdate = new Button(compositeButtons, SWT.NONE);
+		buttonUpdate.setSize(50, 25);
 		buttonUpdate.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
@@ -210,57 +299,46 @@ public class PersonNamePartNavigator {
 	}
 
 	/**
-	 * The object is not needed anymore, but not yet destroyed
-	 */
-	@PreDestroy
-	public void dispose() {
-	}
-
-	/**
+	 * @param context
 	 *
 	 */
-	protected void openNamePartView() {
-		String namePartPid = "0";
-
-		// Open an editor
-		LOGGER.log(Level.FINE, "Opening Name Part View");
-		final ParameterizedCommand command = commandService.createCommand(
-				"net.myerichsen.hremvp.command.opennamepartview", null);
-		handlerService.executeHandler(command);
-
-		final TableItem[] selectedRows = tableViewer.getTable().getSelection();
-
-		if (selectedRows.length > 0) {
-			final TableItem selectedRow = selectedRows[0];
-			namePartPid = selectedRow.getText(0);
+	private void getNewFromDate(IEclipseContext context) {
+		final DateDialog dialog = new DateDialog(textFromDate.getShell(),
+				context);
+		if (dialog.open() == Window.OK) {
+			try {
+				final HDateProvider hdp = new HDateProvider();
+				hdp.setDate(dialog.getDate());
+				hdp.setSortDate(dialog.getSortDate());
+				hdp.setOriginalText(dialog.getOriginal());
+				hdp.setSurety(dialog.getSurety());
+				fromDatePid = hdp.insert();
+				textFromDate.setText(dialog.getDate().toString());
+			} catch (final Exception e) {
+				LOGGER.log(Level.SEVERE, e.toString(), e);
+			}
 		}
-
-		LOGGER.log(Level.INFO, "Setting name part pid: {0}", namePartPid);
-		eventBroker.post(Constants.PERSON_NAME_PART_PID_UPDATE_TOPIC,
-				Integer.parseInt(namePartPid));
 	}
 
 	/**
+	 * @param context
 	 *
 	 */
-	protected void openNameStyleView() {
-		// Open an editor
-		LOGGER.log(Level.FINE, "Opening Name Style View");
-		final ParameterizedCommand command = commandService.createCommand(
-				"net.myerichsen.hremvp.command.opennamestyleview", null);
-		handlerService.executeHandler(command);
-
-		final int nameStylePid = Integer.parseInt(textNameStylePid.getText());
-		LOGGER.log(Level.INFO, "Setting name style pid: {0}", nameStylePid);
-		eventBroker.post(Constants.PERSON_NAME_STYLE_PID_UPDATE_TOPIC,
-				nameStylePid);
-	}
-
-	/**
-	 * The UI element has received the focus
-	 */
-	@Focus
-	public void setFocus() {
+	private void getNewToDate(IEclipseContext context) {
+		final DateDialog dialog = new DateDialog(textToDate.getShell(),
+				context);
+		if (dialog.open() == Window.OK) {
+			try {
+				final HDateProvider hdp = new HDateProvider();
+				hdp.setDate(dialog.getDate());
+				hdp.setSortDate(dialog.getSortDate());
+				hdp.setOriginalText(dialog.getOriginal());
+				hdp.setSurety(dialog.getSurety());
+				toDatePid = hdp.insert();
+			} catch (final Exception e) {
+				LOGGER.log(Level.SEVERE, e.toString(), e);
+			}
+		}
 	}
 
 	/**
@@ -270,7 +348,8 @@ public class PersonNamePartNavigator {
 	@Optional
 	private void subscribeNamePidUpdateTopic(
 			@UIEventTopic(Constants.PERSON_NAME_PID_UPDATE_TOPIC) int personNamePid) {
-		LOGGER.log(Level.FINE, "Received name id " + personNamePid);
+		LOGGER.log(Level.FINE, "Received name id {0}",
+				Integer.toString(personNamePid));
 
 		if (personNamePid == 0) {
 			return;
@@ -281,22 +360,20 @@ public class PersonNamePartNavigator {
 		try {
 			final PersonNameProvider pnp = new PersonNameProvider();
 			pnp.get(personNamePid);
-			textId.setText(Integer.toString(pnp.getNamePid()));
-			textPersonPid.setText(Integer.toString(pnp.getPersonPid()));
-			textNameStylePid.setText(Integer.toString(pnp.getNameStylePid()));
-			textNameStyleLabel.setText(pnp.getNameTypeLabel());
-
-			try {
-				textDateFrom.setText(Integer.toString(pnp.getFromDatePid()));
-			} catch (final Exception e) {
-				textDateFrom.setText("");
-			}
-
-			try {
-				textDateTo.setText(Integer.toString(pnp.getFromDatePid()));
-			} catch (final Exception e) {
-				textDateTo.setText("");
-			}
+//			textNameStylePid.setText(Integer.toString(pnp.getNameStylePid()));
+//			textNameStyleLabel.setText(pnp.getNameTypeLabel());
+//
+//			try {
+//				textDateFrom.setText(Integer.toString(pnp.getFromDatePid()));
+//			} catch (final Exception e) {
+//				textDateFrom.setText("");
+//			}
+//
+//			try {
+//				textDateTo.setText(Integer.toString(pnp.getFromDatePid()));
+//			} catch (final Exception e) {
+//				textDateTo.setText("");
+//			}
 
 			btnPrimaryName.setSelection(pnp.isPrimaryName());
 
@@ -311,15 +388,44 @@ public class PersonNamePartNavigator {
 	 *
 	 */
 	private void update() {
-		// FIXME update
-		// try {
-		// provider = new Names();
-		// provider.setNamePid(Integer.parseInt(textId.getText()));
-		// provider.setPersonPid(Integer.parseInt(textPersonPid.getText()));
-		// provider.setNameType(Integer.parseInt(textNameType.getText()));
-		// provider.update();
-		// } catch (final Exception e) {
-		// e.printStackTrace();
-		// }
+		try {
+//			// Page 2
+//			// Create a new name
+//			final PersonNameProvider personNameProvider = new PersonNameProvider();
+//			personNameProvider.setPersonPid(personPid);
+//			personNameProvider.setNameStylePid(personNameStylePid);
+//			personNameProvider.setFromDatePid(fromDatePid);
+//			personNameProvider.setToDatePid(toDatePid);
+//			personNameProvider.setPrimaryName(true);
+//			final int namePid = personNameProvider.insert();
+//			LOGGER.log(Level.INFO, "Inserted name {0}",
+//					Integer.toString(namePid));
+//
+//			// Page 3
+//			// Create name parts
+//			PersonNamePartProvider pnpp;
+//			String string;
+//			int namePartPid;
+//
+//			// Create each name part
+//			for (int i = 0; i < personNamePartList.size(); i++) {
+//				string = personNamePartList.get(i);
+//
+//				if (string != null) {
+//					pnpp = new PersonNamePartProvider();
+//					pnpp.setNamePid(namePid);
+//					pnpp.setLabel(string);
+//					pnpp.setPartNo(i);
+//					namePartPid = pnpp.insert();
+//					LOGGER.log(Level.INFO, "Inserted name part {0}",
+//							Integer.toString(namePartPid));
+//				}
+//			}
+
+			provider = new PersonNamePartProvider();
+			provider.update();
+		} catch (final Exception e) {
+			LOGGER.log(Level.SEVERE, e.toString(), e);
+		}
 	}
 }
